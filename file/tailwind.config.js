8a7762edaf33739a1170971e4c9b8d2576f5f940
QuetzalJimenez <167280299+QuetzalJimenez@users.noreply.github.com>
2026-05-10 22:38:53 -0600

    Update tailwind.config.js

--- file/tailwind.config.js
+++ file/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./*.html",
+    "./js/**/*.js"
+  ],
+
   theme: {
     extend: {},
   },
+
   plugins: [],
 }
